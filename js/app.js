7e5a8259ad5d00380175819db8ffa2b823831577
mostafizurRahaman <fahim654326@gmail.com>
2022-08-22 02:18:37 +0600

    by button clicked 5 players added and send alert when we want to select more then 5.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
 function fovuritePlayer(player){
    const playerName = player.parentNode.children[0].innerText; 
-   console.log(playerName); 
+   const selectedPlayersContainer = document.getElementById('selected-player'); 
+   const li = document.createElement('li'); 
+   li.innerText = playerName; 
+   
+   if(selectedPlayersContainer.children.length < 5){
+      selectedPlayersContainer.appendChild(li); 
+   }else{
+      alert('You already select 5 players.'); 
+      return; 
+   }
+
+   player.setAttribute("disabled",true)
 }
