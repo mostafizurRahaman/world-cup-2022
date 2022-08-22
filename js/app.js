a2791c9897645ffbd387413d5790e1bf5bd90766
mostafizurRahaman <fahim654326@gmail.com>
2022-08-22 10:24:56 +0600

    full website ready to go

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+/*
+**** funciton for select 5 fovourite player from the players list
+ */
 function fovuritePlayer(player){
    const playerName = player.parentNode.children[0].innerText; 
    const selectedPlayersContainer = document.getElementById('selected-player'); 
@@ -11,5 +14,77 @@ function fovuritePlayer(player){
       return; 
    }
 
-   player.setAttribute("disabled",true)
+   player.setAttribute("disabled",true);
+   player.classList.add('disabled-btn');
 }
+
+/*
+**** get Input Field value and convert the value string to parseFloat  
+*/
+function getInputValue(inputId){
+   const inputField = document.getElementById(inputId); 
+   const inputValueString = inputField.value; 
+   const inputValue = parseFloat(inputValueString); 
+   inputField.value = ""; 
+   return inputValue; 
+}
+
+/* 
+**** get Element Value Function and convert the value string to parseFloat
+*/
+function getElementValue(inputId){
+   const element = document.getElementById(inputId); 
+   const elementValueString= element.innerText; 
+   const elementValue = parseFloat(elementValueString); 
+   return  elementValue; 
+}
+/*
+**** set Element Value Funciton with element Id and newValue;  
+*/
+function setElementValue(elementId , newValue){
+   const element = document.getElementById(elementId); 
+   element.innerText = newValue; 
+}
+
+/*
+**** calculate player expenses 
+**** Validate perPlayer salary
+**** set Value to player expenses
+*/
+document.getElementById('calculate-btn').addEventListener('click', function(){
+   const perPlayerSalary = getInputValue('perPlayer'); 
+   if(isNaN(perPlayerSalary)){
+      alert('Invalid!!!! Please Enter a valid number: '); 
+      return; 
+   }else if(perPlayerSalary <0){
+      alert("Invalid!!!! Please Enter a Positive Number: ")
+   }else{
+     const playerExpenses = perPlayerSalary * 5; 
+     setElementValue('palyerExpenses', playerExpenses); 
+   }
+}); 
+
+/**
+ **** Calculate total expenses 
+ **** Validate managerFee and Coach Fee
+ **** set Total expenses Value
+ */
+document.getElementById('calculate-total').addEventListener('click', function(){
+    const managerFee = getInputValue('manager-fee');     
+    const coachFee = getInputValue('coach-fee');  
+    const playerExpensesString = getElementValue('palyerExpenses'); 
+    if(isNaN(managerFee) ||isNaN(coachFee)){
+         alert('Invalid!!! Please Enter a valid number'); 
+         return; 
+    }else if(managerFee <0 || coachFee <0){
+         alert("Invalid!!! Please Enter a Positive Number: "); 
+         return; 
+     }
+
+    const playerExpenses = parseFloat(playerExpensesString);     
+    const totalExpenses = playerExpenses + coachFee + managerFee; 
+
+    setElementValue("totalExpenses", totalExpenses); 
+}); 
+
+
